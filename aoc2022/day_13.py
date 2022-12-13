0a87471ae12c78f00084f1abbbfcac5748d0772d
Matt Pavord <matt.pavord@krakentechnologies.ltd>
2022-12-13 14:22:41 +0000

    Day 13 - Part 1

---
+++ aoc2022/day_13.py
@@ -0,0 +1,54 @@
+import json
+from typing import Optional, Union
+
+
+def compare(p1: Union[int, list], p2: Union[int, list]) -> Optional[bool]:
+    """
+    Returns True if correct order, False if not, None if identical
+    """
+    if p1 == p2:
+        return None
+    if isinstance(p1, int) and isinstance(p2, int):
+        return p2 > p1
+    if isinstance(p1, int):
+        return compare([p1], p2)
+    if isinstance(p2, int):
+        return compare(p1, [p2])
+    if p1 and not p2:
+        return False
+    if not p1 and p2:
+        return True
+    for i in range(max(len(p1), len(p2))):
+        if len(p1) == i:
+            return True
+        if len(p2) == i:
+            return False
+        result = compare(p1[i], p2[i])
+        if result is not None:
+            return result
+
+
+def task_1(data):
+    total = 0
+    for i, (p1, p2) in enumerate(data, start=1):
+        if compare(p1, p2):
+            total += i
+    return total
+
+
+def task_2(data):
+    return
+
+
+def main():
+    data = []
+    with open('data/day_13.txt', 'rt') as reader:
+        raw_data = reader.read().split("\n\n")
+    for pair in raw_data:
+        data.append([json.loads(x) for x in pair.split("\n")])
+    print(task_1(data))
+    print(task_2(data))
+
+
+if __name__ == "__main__":
+    main()
